21c11d401d1637ce01fa78957573e998bab5d9c2
Anna Avtoneeva <nuta43@mail.ru>
2022-07-25 00:01:45 +0300

    second commit

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,22 +1,24 @@
-let image=document.getElementById("cookie")
-let counter=document.getElementById("clicker__counter")
+let image=document.getElementById("cookie");
+let counter=document.getElementById("clicker__counter");
 
 function count() {
-    let counterElement=counter.textContent
-    counterElement++
-    counter.textContent=counterElement
+    let counterElement=counter.textContent;
+    counterElement++;
+    counter.textContent=counterElement;
 
 }
-image.addEventListener ("click", count)
+image.addEventListener ("click", count);
 
 function changeSizes() {
     if (image.width==200) {
-    image.width=400
-    image.height=250
+    image.width=400;
+    image.height=250;
     }
-    else image.width=200, image.height=128
-            
+    else {
+        image.width=200;
+        image.height=128;
+    }
      }
-    image.addEventListener ("click",changeSizes)
+    image.addEventListener ("click",changeSizes);
     
 
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,33 +1,37 @@
-let deadElement=document.getElementById("dead")
-let lostElement=document.getElementById("lost")
+let deadElement=document.getElementById("dead");
+let lostElement=document.getElementById("lost");
 
 
 
 
 function count() {
-    let deadElement=dead.textContent
-    deadElement++
-    dead.textContent=deadElement
-    if (deadElement===10) alert ("Вы выиграли!")
+    let deadElement=dead.textContent;
+    deadElement++;
+    dead.textContent=deadElement;
+    if (deadElement===10) {
+      alert ("Вы выиграли!")
+    };
 }
 function countLost () {
-    let lostElement=lost.textContent
-    lostElement++
-    lost.textContent=lostElement
-    if (lostElement===5) alert ("Вы проиграли!")
+    let lostElement=lost.textContent;
+    lostElement++;
+    lost.textContent=lostElement;
+    if (lostElement===5) {
+    alert ("Вы проиграли!")
+    };
 }
 function getHole (index) {
-return document.getElementById("hole"+index)
+return document.getElementById("hole"+index);
 }
 
-for (i=1;i<9;i++) {
+for (let i=1;i<10;i++) {
     elementHole = getHole(i);         
     elementHole.addEventListener("click", function() {
         if ( elementHole.className.includes( 'hole_has-mole' ) ) {
           count();
         }
         else {
-         countLost()
+         countLost();
         }
       });
     }
